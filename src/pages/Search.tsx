04fede4186742f2471224cfb76f0d22f630b4e36
rakhmatovdoniyordev <rakhmatovdoniyordev@gmail.com>
2025-04-16 17:26:56 +0500

    Updated Search

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,19 +1,23 @@
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
+import { getThemeStyles } from '../components/themeConfig';
 
 const Search = () => {
     const theme = useSelector((state: RootState) => state.telegram.theme)
+
+    const styles = getThemeStyles(theme);
+
   return (
-    <div className={`${theme === "dark" ? "bg-[#182533] text-white" : "bg-[#D2D2D2] text-black"} h-screen max-w-[450px] mx-auto`}>
-        <form action="" className='px-5 py-4 w-full shadow-[0px_0.33px_0.33px_0px_rgb(118,140,158,0.25)] bg-[#F1F1F1]'>
-            <div className='bg-[#FFFFFF] rounded-[8px] flex items-center px-3'>
+    <div className={`${theme === "dark" ? "bg-[#182533] text-white" : "bg-[#D2D2D2] text-black"} h-screen max-w-[450px] mx-auto pt-[16px]`}>
+        <form action="" className={`px-5 py-4 w-full shadow-[0px_0.33px_0.33px_0px_rgb(118,140,158,0.25)] ${styles.searchHeader}`}>
+            <div className={`rounded-[8px] flex items-center px-3 ${styles.searchInputBg}`}>
                 <span>🔍</span>
                 <input type="text" className='w-full py-2 px-2 outline-none rounded-full font-inter text-sm font-normal tracking-[0.2px] leading-5 text-[#768C9E] placeholder:font-inter' placeholder='Challenge va foydalanuvchilarni izlash'/>
-                <span>&#8652;</span>
+                <button>&#8652;</button>
             </div>
         </form>
         <div className='mx-5 my-2 flex flex-col gap-1'>
-            <div className='bg-white rounded-xl h-auto'>
+            <div className={`rounded-xl h-auto ${styles.searchCardBg}`}>
                 <div className='p-5 flex flex-col gap-5'>
                     <div className='flex gap-3'>
                         <div className='rounded-md flex justify-center items-center'>
@@ -22,11 +26,11 @@ const Search = () => {
                         <div className='flex flex-col gap-3 items-center'>
                             <div className='flex gap-1'>
                                 <span>&#128226;</span>
-                                <p className='font-inter font-bold'>Yo’ldagilar loyihasi</p>
+                                <p className={`font-inter font-bold ${styles.textColor}`}>Yo’ldagilar loyihasi</p>
                             </div>
                             <div className='flex gap-2 items-center'>
                                 <img src="https://media.istockphoto.com/id/1682296067/photo/happy-studio-portrait-or-professional-man-real-estate-agent-or-asian-businessman-smile-for.jpg?s=612x612&w=0&k=20&c=9zbG2-9fl741fbTWw5fNgcEEe4ll-JegrGlQQ6m54rg=" className='rounded-full w-[16px] h-[16px]' alt="" />
-                                <p className='font-inter font-medium text-[12px]'>Muhammad Said</p>
+                                <p className={`font-inter font-medium text-[12px] ${styles.textColor}`}>Muhammad Said</p>
                                 <span className='text-[12px] font-medium text-[#768C9E]'>(Ega)</span>
                             </div>
                         </div>
@@ -38,29 +42,29 @@ const Search = () => {
                         <div className='flex gap-1'>
                             <span>📄</span>
                             <p
-                             className='font-inter font-bold text-sm'>3</p>
+                             className={`font-inter font-bold text-sm ${styles.textColor}`}>3</p>
                         </div>
                         <div className='flex gap-1 items-center'>
                             <span>👁️</span>
                             <p
-                             className='font-inter font-bold text-sm'>700</p>
+                             className={`font-inter font-bold text-sm ${styles.textColor}`}>700</p>
                         </div>
                         <div>
                             <p
-                             className='font-inter font-bold text-sm'><span>&#128101;</span> 700K</p>
+                             className={`font-inter font-bold text-sm ${styles.textColor}`}><span>&#128101;</span> 700K</p>
                         </div>
                         <div>
-                            <p className='font-inter font-bold text-sm'><span className='text-[#FD9400]'>&#9733;</span> 4.5</p>
+                            <p className={`font-inter font-bold text-sm ${styles.textColor}`}><span className='text-[#FD9400]'>&#9733;</span> 4.5</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className={`bg-white rounded-xl p-5 flex`}>
+            <div className={`${styles.searchCardBg} rounded-xl p-5 flex`}>
                 <div className='w-[50px] h-[50px] rounded-full mr-3'>
                     <img src="https://media.istockphoto.com/id/1682296067/photo/happy-studio-portrait-or-professional-man-real-estate-agent-or-asian-businessman-smile-for.jpg?s=612x612&w=0&k=20&c=9zbG2-9fl741fbTWw5fNgcEEe4ll-JegrGlQQ6m54rg=" alt="" className='w-full h-full rounded-full'/>
                 </div>
                 <div className='flex-1 flex flex-col'>
-                    <h3 className='font-inter font-bold'>Muhammad Said <span className='bg-custom-gradient px-1 text-white'> &#10004; </span></h3>
+                    <h3 className={`${styles.textColor} font-inter font-bold`}>Muhammad Said <span className='bg-custom-gradient px-1 text-white'> &#10004; </span></h3>
                     <div className='flex items-center gap-2'>
                         <a href="" className='text-[12px] font-inter  text-[#768C9E]'>@Muhammad_Said</a>
                         <span className='text-[#52DAFF]'>&#9992;</span>
